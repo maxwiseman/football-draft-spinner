fbe402385704721373e7b5b77167c4b1ae5c25a9
maxwiseman <13maxwellwise@gmail.com>
2023-10-17 19:16:33 -0400

    feat: change bg when using dark mode, resolves #2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>
+      <body className={`font-sans ${inter.variable} dark:bg-gray-800`}>
         <TRPCReactProvider headers={headers()}>{children}</TRPCReactProvider>
       </body>
     </html>
