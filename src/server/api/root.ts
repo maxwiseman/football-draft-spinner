8332c8624a69043caa9bba13f776d4f7ef2eb5d3
maxwiseman <13maxwellwise@gmail.com>
2023-10-25 14:18:02 -0400

    fix: fixed build error

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from '@/server/api/routers/post';
 import { createTRPCRouter } from '@/server/api/trpc';
 import { espnRouter } from './routers/espn';
 
@@ -8,7 +7,6 @@ import { espnRouter } from './routers/espn';
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   espn: espnRouter,
 });
 
