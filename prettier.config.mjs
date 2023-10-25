87c9408383aed6c2a2dfe61dd7aa7c341e3f7830
maxwiseman <13maxwellwise@gmail.com>
2023-10-25 15:13:46 -0400

    chore: removed old prettier options

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
-  "importOrder": ["^components/(.*)$", "^[./]"],
-  "importOrderSeparation": true,
-  "importOrderSortSpecifiers": true
 };
 
 export default config;
